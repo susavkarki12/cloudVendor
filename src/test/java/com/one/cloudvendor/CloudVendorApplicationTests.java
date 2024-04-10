4845d6f321e36de87f73b61854c078dd40ba80d5
susavkarki12 <077bei043@ioepc.edu.np>
2024-04-10 20:49:10 +0545

    initial commit

---
+++ src/test/java/com/one/cloudvendor/CloudVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.cloudvendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudVendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
